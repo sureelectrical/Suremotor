41cb0c62676c572d2161a5fdef95abc8ba11d9f4
sure <oat_n@yahoo.com>
2018-09-08 16:06:03 +0700

    Finish 8stp

--- app/src/main/java/com/oat_nyahoo/sure_motor/MainActivity.java
+++ app/src/main/java/com/oat_nyahoo/sure_motor/MainActivity.java
@@ -9,5 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//       Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment()).commit();
+
+        }
+
+
+    }  // Main Method
+
+}  // Main Class
